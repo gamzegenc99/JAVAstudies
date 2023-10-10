dc2a73838e42e1b6750bc185e5f952123a1f1ab3
Gamze Genç <81413142+gamzegenc99@users.noreply.github.com>
2023-10-11 00:34:36 +0300

    For loops

---
+++ Study_6.java
@@ -0,0 +1,21 @@
+package study_6;
+
+/**
+ *
+ * @author gamze
+ */
+public class Study_6 {
+    
+    public static void main(String[] args) {
+        // for (baslatma ; kosul ; artırma veya azaltma işlemleri) {
+          //  Döngü koşulumuz, doğru olduğu sürece burası çalışacak.
+        
+   for (int i = 0; i < 5 ; i++) {
+          System.out.println("i = " + i);
+          
+      } 
+
+        
+    }
+    
+}
