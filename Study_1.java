7c637cd6a18d5caba7a8612acd37d9fe24397ee6
Gamze Genç <81413142+gamzegenc99@users.noreply.github.com>
2023-10-11 00:21:07 +0300

    Defining and outputting char and boolean

---
+++ Study_1.java
@@ -0,0 +1,25 @@
+
+
+package study_1; 
+
+
+
+public class Study_1 {
+
+    public static void main(String[] args) {
+        char karakter1 = 89;
+        char karakter2 = 'H';
+        System.out.println(" karakter1 "  +   karakter1 + 
+                
+                " karakter2 " + karakter2 );
+        boolean a = true;
+        boolean b = false;
+     
+              System.out.println(a);
+              
+                 System.out.println(b);
+                 
+                
+                }
+    
+}
